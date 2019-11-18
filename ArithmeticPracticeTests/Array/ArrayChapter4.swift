e76fdd7558b8d685e2075ff16074b69d1001bf58
Tsuf <tifatsubasa@gmail.com>
2019-11-18 16:33:40 +0800

    feat: twoSum & removeElement

--- ArithmeticPracticeTests/Array/ArrayChapter4.swift
+++ ArithmeticPracticeTests/Array/ArrayChapter4.swift
@@ -35,4 +35,67 @@ class ArrayChapter4: XCTestCase {
         reverseString(&b)
         XCTAssertEqual(b, ["h","a","n","n","a","H"])
     }
+    
+    func testArrayPairSum() {
+        func arrayPairSum(_ nums: [Int]) -> Int {
+            let count = nums.count
+            if count == 0 { return 0 }
+            guard count % 2 == 0 else { return 0 }
+            let sortNums = nums.sorted(by: { $0 < $1 })
+            var sum = 0
+            var index = 0
+            while index < count {
+                sum += sortNums[index]
+                index += 2
+            }
+            return sum
+        }
+        XCTAssertEqual(arrayPairSum([1,4,3,2]), 4)
+    }
+    
+    func testTwoSum() {
+        func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+            let count = numbers.count
+            if count < 2 { return [] }
+            var i = 0
+            var j = count - 1
+            while i < j {
+                let sum = numbers[i] + numbers[j]
+                if sum == target {
+                    return [i + 1, j + 1]
+                }
+                if sum < target {
+                    i += 1
+                } else if sum > target {
+                    j -= 1
+                }
+            }
+            return []
+        }
+        XCTAssertEqual(twoSum([-1, 0], -1), [1, 2])
+        XCTAssertEqual(twoSum([2, 7, 11, 15], 9), [1, 2])
+        XCTAssertEqual(twoSum([2, 7, 11, 15], 18), [2, 3])
+        XCTAssertEqual(twoSum([1,2,3,4,4,9,56,90], 8), [4, 5])
+    }
+    
+    func testRemoveElement() {
+        func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+            if nums.count == 0 { return 0 }
+            var i = 0
+            while i < nums.count {
+                if nums[i] == val {
+                    nums.remove(at: i)
+                } else {
+                    i += 1
+                }
+            }
+            return nums.count
+        }
+        var a = [3,2,2,3]
+        var b = [0,1,2,2,3,0,4,2]
+        var c = [2, 2]
+        XCTAssertEqual(removeElement(&a, 3), 2)
+        XCTAssertEqual(removeElement(&b, 2), 5)
+        XCTAssertEqual(removeElement(&c, 2), 0)
+    }
 }
