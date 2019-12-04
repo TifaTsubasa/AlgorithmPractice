1c38a8ccba4e880c3b304011784604fb30c960bd
Tsuf <tifatsubasa@gmail.com>
2019-12-04 18:11:01 +0800

    merge sort

--- ArithmeticPracticeTests/Practice/Sort.swift
+++ ArithmeticPracticeTests/Practice/Sort.swift
@@ -9,99 +9,151 @@
 import XCTest
 
 class Sort: XCTestCase {
+  
+  let test1: ([Int], [Int]) = ([], [])
+  let test2 = ([4,5,6,3,2,1], [1,2,3,4,5,6])
+  let test3 = ([1], [1])
+  
+  func testBubbleSort() {
+    func bubbleSort(_ nums: inout [Int]) {
+      let count = nums.count
+      if count <= 1 { return }
+      var hasExchange = false
+      for i in 0..<(count - 1) {
+        for j in 0..<(count - i - 1) {
+          if nums[j] > nums[j + 1] {
+            let tmp = nums[j + 1]
+            nums[j + 1] = nums[j]
+            nums[j] = tmp
+            hasExchange = true
+          }
+        }
+        if hasExchange == false { break }
+      }
+    }
+    var arr1 = test1.0
+    bubbleSort(&arr1)
+    XCTAssertEqual(arr1, test1.1)
+    var arr2 = test2.0
+    bubbleSort(&arr2)
+    XCTAssertEqual(arr2, test2.1)
+    var arr3 = test3.0
+    bubbleSort(&arr3)
+    XCTAssertEqual(arr3, test3.1)
+  }
+  
+  func testInsertSort() {
+    func insertSort(_ nums: inout [Int]) {
+      let count = nums.count
+      if count <= 1 { return }
+      for i in 1..<count {
+        let value = nums[i]
+        var endIndex = -1
+        for j in 0..<i {
+          let index = i - j - 1
+          print(index)
+          if nums[index] > value {
+            nums[index + 1] = nums[index]
+            print(nums)
+          } else {
+            endIndex = index
+            break
+          }
+        }
+        nums[endIndex + 1] = value
+        print(nums)
+      }
+    }
+    var arr1 = test1.0
+    insertSort(&arr1)
+    XCTAssertEqual(arr1, test1.1)
+    var arr2 = test2.0
+    insertSort(&arr2)
+    XCTAssertEqual(arr2, test2.1)
+    var arr3 = test3.0
+    insertSort(&arr3)
+    XCTAssertEqual(arr3, test3.1)
+  }
+  
+  func testSelectSort() {
+    func selectSort(_ nums: inout [Int]) {
+      let count = nums.count
+      if count <= 1 { return }
+      for i in 0..<(count - 1) {
+        var minIndex = i
+        var min = nums[i]
+        for j in i..<count {
+          if min > nums[j] {
+            min = nums[j]
+            minIndex = j
+          }
+        }
+        let tmp = nums[i]
+        nums[i] = nums[minIndex]
+        nums[minIndex] = tmp
+        print(nums)
+      }
+    }
+    var arr1 = test1.0
+    selectSort(&arr1)
+    XCTAssertEqual(arr1, test1.1)
+    var arr2 = test2.0
+    selectSort(&arr2)
+    XCTAssertEqual(arr2, test2.1)
+    var arr3 = test3.0
+    selectSort(&arr3)
+    XCTAssertEqual(arr3, test3.1)
+  }
+  
+  func testMergeSort() {
+    func merge(_ nums: inout [Int], l: Int, p: Int, r: Int) {
+      var array = [Int]()
+      var left = l
+      var right = p + 1
+      while array.count <= (r - l) {
+        if left > p {
+          array.append(nums[right])
+          right += 1
+        } else if right > r {
+          array.append(nums[left])
+          left += 1
+        } else {
+          if nums[left] <= nums[right] {
+            array.append(nums[left])
+            left += 1
+          } else {
+            array.append(nums[right])
+            right += 1
+          }
+        }
+      }
+      var point = l
+      for i in 0..<(array.count) {
+        nums[point] = array[i]
+        point += 1
+      }
+    }
     
-    let test1: ([Int], [Int]) = ([], [])
-    let test2 = ([4,5,6,3,2,1], [1,2,3,4,5,6])
-    let test3 = ([1], [1])
-
-    func testBubbleSort() {
-        func bubbleSort(_ nums: inout [Int]) {
-            let count = nums.count
-            if count <= 1 { return }
-            var hasExchange = false
-            for i in 0..<(count - 1) {
-                for j in 0..<(count - i - 1) {
-                    if nums[j] > nums[j + 1] {
-                        let tmp = nums[j + 1]
-                        nums[j + 1] = nums[j]
-                        nums[j] = tmp
-                        hasExchange = true
-                    }
-                }
-                if hasExchange == false { break }
-            }
-        }
-        var arr1 = test1.0
-        bubbleSort(&arr1)
-        XCTAssertEqual(arr1, test1.1)
-        var arr2 = test2.0
-        bubbleSort(&arr2)
-        XCTAssertEqual(arr2, test2.1)
-        var arr3 = test3.0
-        bubbleSort(&arr3)
-        XCTAssertEqual(arr3, test3.1)
-    }
-
-    func testInsertSort() {
-        func insertSort(_ nums: inout [Int]) {
-            let count = nums.count
-            if count <= 1 { return }
-            for i in 1..<count {
-                let value = nums[i]
-                var endIndex = -1
-                for j in 0..<i {
-                    let index = i - j - 1
-                    print(index)
-                    if nums[index] > value {
-                        nums[index + 1] = nums[index]
-                        print(nums)
-                    } else {
-                        endIndex = index
-                        break
-                    }
-                }
-                nums[endIndex + 1] = value
-                print(nums)
-            }
-        }
-        var arr1 = test1.0
-        insertSort(&arr1)
-        XCTAssertEqual(arr1, test1.1)
-        var arr2 = test2.0
-        insertSort(&arr2)
-        XCTAssertEqual(arr2, test2.1)
-        var arr3 = test3.0
-        insertSort(&arr3)
-        XCTAssertEqual(arr3, test3.1)
+    func mergeSort(_ nums: inout [Int], l: Int, r: Int) {
+      let count = nums.count
+      if count <= 1 { return }
+      if l >= r { return }
+      let center = (r - l) / 2 + l
+      print(l, r)
+      mergeSort(&nums, l: l, r: center)
+      mergeSort(&nums, l: center + 1, r: r)
+      merge(&nums, l: l, p: center, r: r)
     }
     
-    func testSelectSort() {
-        func selectSort(_ nums: inout [Int]) {
-            let count = nums.count
-            if count <= 1 { return }
-            for i in 0..<(count - 1) {
-                var minIndex = i
-                var min = nums[i]
-                for j in i..<count {
-                    if min > nums[j] {
-                        min = nums[j]
-                        minIndex = j
-                    }
-                }
-                let tmp = nums[i]
-                nums[i] = nums[minIndex]
-                nums[minIndex] = tmp
-                print(nums)
-            }
-        }
-        var arr1 = test1.0
-        selectSort(&arr1)
-        XCTAssertEqual(arr1, test1.1)
-        var arr2 = test2.0
-        selectSort(&arr2)
-        XCTAssertEqual(arr2, test2.1)
-        var arr3 = test3.0
-        selectSort(&arr3)
-        XCTAssertEqual(arr3, test3.1)
-    }
+    
+    var arr1 = test1.0
+    mergeSort(&arr1, l: 0, r: arr1.count - 1)
+    XCTAssertEqual(arr1, test1.1)
+    var arr2 = test2.0
+    mergeSort(&arr2, l: 0, r: arr2.count - 1)
+    XCTAssertEqual(arr2, test2.1)
+    var arr3 = test3.0
+    mergeSort(&arr3, l: 0, r: arr3.count - 1)
+    XCTAssertEqual(arr3, test3.1)
+  }
 }
