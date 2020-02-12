2995ac3df55a07c60a2303c4a25b27db1472a94a
Tsuf <tifatsubasa@gmail.com>
2020-02-12 12:29:30 +0800

    no.121

--- ArithmeticPracticeTests/LeetCode/100-200/n121.swift
+++ ArithmeticPracticeTests/LeetCode/100-200/n121.swift
@@ -12,6 +12,21 @@ class n121: XCTestCase {
     
     func maxProfit(_ prices: [Int]) -> Int {
         if prices.count <= 1 { return 0 }
+        var dp = [[Int]]()
+        for _ in 0..<prices.count {
+            dp.append([0, 0])
+        }
+        dp[0][0] = 0
+        dp[0][1] = -prices[0]
+        for i in 1..<prices.count {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+            dp[i][1] = max(dp[i - 1][1], -prices[i])
+        }
+        return dp[prices.count - 1][0]
+    }
+    
+    func maxProfit1(_ prices: [Int]) -> Int {
+        if prices.count <= 1 { return 0 }
         var minPrice = prices[0]
         var res = 0
         for i in 1..<prices.count {
@@ -23,12 +38,12 @@ class n121: XCTestCase {
     }
 
     func testExample() {
-        XCTAssertEqual(maxProfit([]), 0)
-        XCTAssertEqual(maxProfit([1]), 0)
+//        XCTAssertEqual(maxProfit([]), 0)
+//        XCTAssertEqual(maxProfit([1]), 0)
         XCTAssertEqual(maxProfit([7,1,5,3,6,4]), 5)
-        XCTAssertEqual(maxProfit([7,6,4,3,1]), 0)
-        XCTAssertEqual(maxProfit([1, 3, 4, 2]), 3)
-        XCTAssertEqual(maxProfit([7,2,5,1,3]), 3)
+//        XCTAssertEqual(maxProfit([7,6,4,3,1]), 0)
+//        XCTAssertEqual(maxProfit([1, 3, 4, 2]), 3)
+//        XCTAssertEqual(maxProfit([7,2,5,1,3]), 3)
     }
 
 }
---
+++ ArithmeticPracticeTests/LeetCode/400-500/n442.swift
@@ -0,0 +1,19 @@
+//
+//  n442.swift
+//  AlgorithmPracticeTests
+//
+//  Created by tsuf on 2020/2/12.
+//  Copyright © 2020 upmer. All rights reserved.
+//
+
+import XCTest
+
+class n442: XCTestCase {
+    
+    func findDuplicates(_ nums: [Int]) -> [Int] {
+        return []
+    }
+
+    func testExample() {
+    }
+}
